4455aa6f84ff7de8e64e452d6bc9035567f6a471
William Eliot <75908561+wjeliot@users.noreply.github.com>
2022-08-04 18:07:42 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# vanilla-gpt
+
+Minimal, annotated implementation of GPT in PyTorch. STILL IN DEVELOPMENT
---
+++ tests/test_vanilla_gpt.py
@@ -0,0 +1,5 @@
+from vanilla_gpt import __version__
+
+
+def test_version():
+    assert __version__ == '0.1.0'
